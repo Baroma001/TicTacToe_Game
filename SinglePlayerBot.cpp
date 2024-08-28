38c8599888ac318c74c42f6ea78cbfc50b175e5b
Baroma001 <49646668+Baroma001@users.noreply.github.com>
2024-08-27 20:48:17 -0500

    Added another Header and Source File

---
+++ SinglePlayerBot.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Yannick Hettinga on 27.08.24.
+//
+
+#include "SinglePlayerBot.h"
---
+++ SinglePlayerBot.h
@@ -0,0 +1,8 @@
+//
+// Created by Yannick Hettinga on 27.08.24.
+//
+
+#ifndef SINGLEPLAYERBOT_H
+#define SINGLEPLAYERBOT_H
+
+#endif //SINGLEPLAYERBOT_H
